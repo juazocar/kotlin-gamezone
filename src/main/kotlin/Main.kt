20c5146fd95be6c1ef8be35d91024c759271cd33
juazocar <juazocar@profesor.duoc.cl>
2026-09-03 17:27:18 -0400

    KOT-002 - Agregar variables y tipos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,22 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val nombreSistema: String = "GameZone"
+    val capacidad: Int = 10
+    var recaudacionTotal: Double = 0.0
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("Sistema: $nombreSistema")
+    println("Capacidad: $capacidad")
+    println("Recaudación: $recaudacionTotal")
+
+    recaudacionTotal = 800.0
+
+    println("Nueva recaudación: $recaudacionTotal")
+
+    val codigoConsola: String = "CC12CD"
+    val marca: String = "PlayStation"
+    val modelo: String = "PlayStation 5"
+    val minutosUso: Int = 75
+    val tarifaHora: Double = 800.0
+
 }
